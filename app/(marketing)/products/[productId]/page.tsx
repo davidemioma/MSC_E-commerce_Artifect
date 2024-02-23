1c7abc7dcb84150744c41edf4f07c3a8d5b81749
david emioma <ue22aac@herts.ac.uk>
2024-02-23 21:05:24 +0000

    Deployment issues

--- app/(marketing)/products/[productId]/page.tsx
+++ app/(marketing)/products/[productId]/page.tsx
@@ -86,6 +86,9 @@ export default async function ProductPage({
         },
         include: {
           availableItems: {
+            include: {
+              size: true,
+            },
             orderBy: {
               createdAt: "desc",
             },
