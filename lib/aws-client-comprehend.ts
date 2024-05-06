4e832f3d0f3ecb13a4cc384c4aaf410ea100aea1
david emioma <ue22aac@herts.ac.uk>
2024-05-06 22:04:17 +0100

    Changed AWS keys

--- lib/aws-client-comprehend.ts
+++ lib/aws-client-comprehend.ts
@@ -3,7 +3,7 @@ import { ComprehendClient } from "@aws-sdk/client-comprehend";
 export const comprehendClient = new ComprehendClient({
   region: process.env.AWS_REGION || "",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY || "",
+    accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   },
 });
--- lib/aws-client-rekognition.ts
+++ lib/aws-client-rekognition.ts
@@ -3,7 +3,7 @@ import { RekognitionClient } from "@aws-sdk/client-rekognition";
 export const rekognitionClient = new RekognitionClient({
   region: process.env.AWS_REGION || "",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY || "",
+    accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   },
 });
