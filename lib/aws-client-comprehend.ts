fea21a83ad0ce9002004521103ed131d4b0cda1f
david emioma <ue22aac@herts.ac.uk>
2024-05-06 22:15:03 +0100

    Deployment issues

--- lib/aws-client-comprehend.ts
+++ lib/aws-client-comprehend.ts
@@ -1,7 +1,7 @@
 import { ComprehendClient } from "@aws-sdk/client-comprehend";
 
 export const comprehendClient = new ComprehendClient({
-  region: process.env.AWS_REGION || "",
+  region: process.env.AWS_REGION || "eu-west-2",
   credentials: {
     accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
--- lib/aws-client-rekognition.ts
+++ lib/aws-client-rekognition.ts
@@ -1,7 +1,7 @@
 import { RekognitionClient } from "@aws-sdk/client-rekognition";
 
 export const rekognitionClient = new RekognitionClient({
-  region: process.env.AWS_REGION || "",
+  region: process.env.AWS_REGION || "eu-west-2",
   credentials: {
     accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
